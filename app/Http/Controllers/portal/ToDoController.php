39ffb835a0b087c78b8019e27172b8d427475e42
AbdelRahman Wahdan <a_wahdan93@hotmail.com>
2017-11-18 19:18:15 +0200

    initial commit

---
+++ app/Http/Controllers/portal/ToDoController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\portal;
+
+
+use App\Http\Controllers\Controller;
+
+class ToDoController extends Controller
+{
+public function index()
+{
+    return View('todo');
+}
+	
+	
+}
---
+++ resources/views/layout/default_layout.blade.php
@@ -0,0 +1,19 @@
+<html>
+<!--
+reference : https://bootsnipp.com/snippets/featured/todo-example
+-->
+<title>ToDo list</title>
+<head>
+{{ Html::style('css/bootstrap.min.css') }}
+{{ Html::style('css/todo.css') }}
+
+</head>
+<body>
+@yield('content')
+{{ Html::script('js/jquery-3.2.1.min.js') }}
+{{ Html::script('js/jquery-ui.min.js') }}
+{{ Html::script('js/bootstrap.min.js') }}
+{{ Html::script('js/todo.js') }}
+
+</body>
+</html>
